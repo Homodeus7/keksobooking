64e100109277d382a21c1e854a5c337e4a789a6f
homodeus7 <plum.bus@yahoo.com>
2022-11-02 17:35:10 +0300

    add event for selector #timeline

--- js/form.js
+++ js/form.js
@@ -1,22 +1,36 @@
-const selectorType = document.querySelector(`#type`);
-const inputPrise = document.querySelector(`#price`);
+const selectorType = document.querySelector(`#type`)
+const inputPrise = document.querySelector(`#price`)
+const selectorTimeIn = document.querySelector(`#timein`)
+const selectorTimeOut = document.querySelector(`#timeout`)
 
 selectorType.addEventListener('change', (evt) => {
     const item = evt.target.value;
 
     switch (item) {
-        case `bungalow`: inputPrise.min = 0;
+        case `bungalow`: inputPrise.min = `0`;
             break
-        case `flat`: inputPrise.min = 1000;
+        case `flat`: inputPrise.min = `1000`;
             break
-        case `houtel`: inputPrise.min = 3000;
+        case `houel`: inputPrise.min = `3000`;
             break
-        case `house`: inputPrise.min = 5000;
+        case `house`: inputPrise.min = `5000`;
             break
-        case `palace`: inputPrise.min = 10000;
+        case `palace`: inputPrise.min = `10000`;
             break
     }
-    console.log(inputPrise.min)
+})
+
+selectorTimeIn.addEventListener('change', (evt) => {
+    const item = evt.target.value
+
+    switch (item) {
+        case `12:00`: selectorTimeOut.value = `12:00`;
+            break
+        case `13:00`: selectorTimeOut.value = `13:00`;
+            break
+        case `14:00`: selectorTimeOut.value = `14:00`;
+            break
+    }
 })
 
 
