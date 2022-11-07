3f83e56df18aa253f659ab4d8675cf6cf132014a
homodeus7 <plum.bus@yahoo.com>
2022-11-07 16:44:33 +0300

    add data.json

--- js/map.js
+++ js/map.js
@@ -19,7 +19,7 @@ const map = L.map('map-canvas')
 L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     },
 ).addTo(map);
 
